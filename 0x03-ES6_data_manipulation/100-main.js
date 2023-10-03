7f0643d597087b48a83fdc24c7eaf2238044541e
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2023-10-03 23:56:24 +0100

    Update 100-main.js

--- 0x03-ES6_data_manipulation/100-main.js
+++ 0x03-ES6_data_manipulation/100-main.js
@@ -1,1 +1,15 @@
-jjk
+import { queryAPI, weakMap } from './100-weak';
+
+const endpoint = { protocol: 'http', name: 'getUsers' };
+weakMap.get(endpoint);
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+console.log(weakMap.get(endpoint));
+
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
+queryAPI(endpoint);
