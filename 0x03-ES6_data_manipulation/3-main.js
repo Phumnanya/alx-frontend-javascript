8c42d5ceafa9d5ecefc6e3824453ed39770522a3
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2023-10-03 23:59:37 +0100

    Update 3-main.js

--- 0x03-ES6_data_manipulation/3-main.js
+++ 0x03-ES6_data_manipulation/3-main.js
@@ -1,1 +1,7 @@
-km
+import getListStudents from './0-get_list_students';
+import getStudentIdsSum from './3-get_ids_sum';
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+
+console.log(value);
